7a58d0c58e5e031f0735efac52315fe726aeb227
PintoDev64 <cardozojoan13@gmail.com>
2025-03-24 17:39:30 -0500

    Definiendo base del engine

---
+++ packages/engine/src/classes.ts
@@ -0,0 +1,3 @@
+export interface INovelJsEngine {
+    Start(): Promise<void>
+}
---
+++ packages/engine/src/classes/NovelJsEngine.tsx
@@ -0,0 +1,13 @@
+import { INovelJsEngine } from "../classes";
+
+export default class NovelJsEngine implements INovelJsEngine {
+    private constructor() {}
+
+    static GetInstance() {
+
+    }
+
+    async Start() {
+        
+    }
+}
